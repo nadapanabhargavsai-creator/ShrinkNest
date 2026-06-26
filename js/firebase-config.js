58ef3009476f098fadd77b7f9868b0737c718488
nadapanabhargavsai <nadapanabhargavsai@gmail.com>
2026-06-26 17:15:34 +0530

    edited all  files

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -20,7 +20,21 @@ import {
 // Replace this with YOUR Firebase Config
 // ==========================================
 
+const firebaseConfig = {
 
+    apiKey: "YOUR_API_KEY",
+
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+    projectId: "YOUR_PROJECT_ID",
+
+    storageBucket: "YOUR_PROJECT.appspot.com",
+
+    messagingSenderId: "YOUR_SENDER_ID",
+
+    appId: "YOUR_APP_ID"
+
+};
 
 
 // ==========================================
