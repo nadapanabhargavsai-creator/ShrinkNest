3e652f3aeaba0b06226cf52976953518886235ed
np9343-ai <np9343@srmist.edu.in>
2026-06-26 17:15:05 +0530

    edited all files

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -20,21 +20,7 @@ import {
 // Replace this with YOUR Firebase Config
 // ==========================================
 
-const firebaseConfig = {
 
-    apiKey: "YOUR_API_KEY",
-
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-
-    projectId: "YOUR_PROJECT_ID",
-
-    storageBucket: "YOUR_PROJECT.appspot.com",
-
-    messagingSenderId: "YOUR_SENDER_ID",
-
-    appId: "YOUR_APP_ID"
-
-};
 
 
 // ==========================================
